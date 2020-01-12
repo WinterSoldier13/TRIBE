4c3fc59e3dd1f6295a0de1ae3c083ca45526567d
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-01-12 22:07:34 +0530

    Added sample pages for the apps

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def test_request(request):
+    return HttpResponse('<h1>This is the HOME page</h1>')
