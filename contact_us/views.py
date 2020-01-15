1d49e4c27db2fdd25853164ea11cc76bb2749526
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-01-16 01:34:25 +0530

    Added contact_us app

--- contact_us/views.py
+++ contact_us/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def load(request):
-    return render(request, 'contact_us/test.html')
+    return render(request, 'contact_us/contact.html')
