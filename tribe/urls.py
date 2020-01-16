1aabc13c2077cc0a298190c7b067645e8a70a9bf
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-01-16 23:15:01 +0530

    fixed a minor bug related to products app

--- tribe/urls.py
+++ tribe/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',include('home.urls')),
 
-    path('product_test/',include('product.urls')),
+
     path('accounts/', include('accounts.urls')),
     
 
